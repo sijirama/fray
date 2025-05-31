9a4251bb045a380214d4f7db89b867e0cba28872
sijirama <gbemilesanmi@gmail.com>
2025-05-31 07:35:17 +0100

    fix: prevent cylces in connections

--- components/blocks/Editor/FlowEditor.tsx
+++ components/blocks/Editor/FlowEditor.tsx
@@ -9,6 +9,7 @@ import {
   Connection,
   Controls,
   Edge,
+  getOutgoers,
   ReactFlow,
   useEdgesState,
   useNodesState,
@@ -98,37 +99,53 @@ export default function FlowEditor(props: Props) {
     [setEdges, updateNodeData, nodes],
   );
 
-  const isValidConnection = useCallback((connection: Edge | Connection) => {
-    // no self connection
-    if (connection.source == connection.target) {
-      return false;
-    }
+  const isValidConnection = useCallback(
+    (connection: Edge | Connection) => {
+      // no self connection
+      if (connection.source == connection.target) {
+        return false;
+      }
 
-    // same taskParam type connection
-    const source = nodes.find((node) => node.id === connection.source);
-    const target = nodes.find((node) => node.id === connection.target);
-    if (!source || !target) {
-      console.error("invalid connection: source or target node not found");
-      return false;
-    }
+      // same taskParam type connection
+      const source = nodes.find((node) => node.id === connection.source);
+      const target = nodes.find((node) => node.id === connection.target);
+      if (!source || !target) {
+        console.error("invalid connection: source or target node not found");
+        return false;
+      }
 
-    const sourceTask = TaskRegistry[source.data.type];
-    const targetTask = TaskRegistry[target.data.type];
+      const sourceTask = TaskRegistry[source.data.type];
+      const targetTask = TaskRegistry[target.data.type];
 
-    const output = sourceTask.outputs.find(
-      (o) => o.name === connection.sourceHandle,
-    );
-    const input = targetTask.inputs.find(
-      (o) => o.name === connection.targetHandle,
-    );
+      const output = sourceTask.outputs.find(
+        (o) => o.name === connection.sourceHandle,
+      );
+      const input = targetTask.inputs.find(
+        (o) => o.name === connection.targetHandle,
+      );
 
-    if (input?.type !== output?.type) {
-      console.error("Invalid connection: type mismatch");
-      return false;
-    }
+      if (input?.type !== output?.type) {
+        console.error("Invalid connection: type mismatch");
+        return false;
+      }
 
-    return true;
-  }, []);
+      // prevent cycles
+      const hasCycle = (node: AppNode, visited = new Set()) => {
+        if (visited.has(node.id)) return false;
+        visited.add(node.id);
+
+        for (const outgoer of getOutgoers(node, nodes, edges)) {
+          if (outgoer.id === connection.source) return true;
+          if (hasCycle(outgoer, visited)) return true;
+        }
+      };
+
+      const detectedCycle = hasCycle(target);
+
+      return !detectedCycle;
+    },
+    [nodes, edges],
+  );
 
   return (
     <main className="h-full w-full">
